a94ced2e0e15715dc8d4e24b02d7b12172c2daae
tsskiracer <william.starkey@MU-C1-03.mullen.pvt.k12.co.us>
2015-09-29 14:09:24 -0600

    in progress

--- Card Games/src/Players.java
+++ Card Games/src/Players.java
@@ -1,17 +1,43 @@
 import java.util.ArrayList;
-
+import java.util.Scanner;
 public class Players {
 	private int bank;
 	private int bet;
 	static ArrayList<Card> hand = new ArrayList<Card>();
 	
-	public Players(int b, int bt, Card c1, Card c2)
+	public Players(int b, int bt)
 	{
 	bank=b;
 	bet=bt;
-	
 	}
 	
+	public int getBank()
+		{
+			return bank;
+		}
+
+	public void setBank(int bank)
+		{
+			this.bank = bank;
+		}
+
+	public int getBet()
+		{
+			return bet;
+		}
+
+	public void setBet(int bet)
+		{
+			this.bet = bet;
+		}
+
+	public static void playerMoney()
+	{
+		Scanner userBet=new Scanner(System.in);
+		int bet=userBet.nextInt();
+		hand.add(new Card(deck[]));
+		
+	}
 
 	public static void main(String[] args) {
 		
